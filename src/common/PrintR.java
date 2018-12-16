f015b7557fd150ba573a7df596ff08e1cf593077
cuiyaocool <cuiyaovip@126.com>
2018-12-16 17:16:08 +0800

    common

---
+++ src/common/PrintR.java
@@ -0,0 +1,11 @@
+package common;
+
+public class PrintR {
+	public static void Print(int[] r) {
+		// TODO Auto-generated method stub
+		for (int i : r) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+}
